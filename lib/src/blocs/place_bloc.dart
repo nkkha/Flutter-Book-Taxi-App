75744d24566714fcb5a8f3ef8a9e33aea97bbf6b
nkkhaityu <45230159+nkkhaityu@users.noreply.github.com>
2020-02-20 10:54:21 +0700

    Add google maps search features

---
+++ lib/src/blocs/place_bloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'dart:async';
+import 'package:flutter_taxiapp/src/repository/place_service.dart';
+
+class PlaceBloc {
+  var _placeController = StreamController();
+
+  Stream get placeStream => _placeController.stream;
+
+  void searchPlace(String keyword) {
+    print("place bloc search: " + keyword);
+
+    _placeController.sink.add("start");
+    PlaceService.searchPlace(keyword).then((rs) {
+      _placeController.sink.add(rs);
+    }).catchError(() {
+//      _placeController.sink.add("stop");
+    });
+  }
+
+  void dispose() {
+    _placeController.close();
+  }
+}
---
+++ lib/src/config/configs.dart
@@ -0,0 +1,4 @@
+
+class Configs {
+  static String ggKEY = "AIzaSyDpVusO4Bw8HuNkKIU8OUZT4V05yjLVz7g";
+}
